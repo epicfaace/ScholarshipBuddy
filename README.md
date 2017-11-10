4a94349f830dfa578832fbdbc1b3b452c1963643
Ashwin Ramaswami <aramaswamis@gmail.com>
2017-11-10 14:48:47 -0800

    redirect home url to /apply

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# DjangoAzure [![Deploy to Azure](http://azuredeploy.net/deploybutton.png)](https://azuredeploy.net/)
+# IASF apply website
+Website for the Indian American Scholarship Foundation application.
+URL: http://apply.iasf.org/
 
 Django makes it easier to build better Web apps more quickly and with less code.[Get started with Django](https://www.djangoproject.com/start/)
 
--- iasf/urls.py
+++ iasf/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^apply/', include('iasf.apply.urls')),
-    url(r'^accounts/', include('iasf.accounts.urls'))
+    url(r'^accounts/', include('iasf.accounts.urls')),
+    url(r'^', RedirectView.as_view(url='/apply/', permanent=True))
 ]
