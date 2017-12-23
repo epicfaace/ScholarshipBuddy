1f131ffff23d59c02593d146a6ed9a4c78a8fec3
Ashwin Ramaswami <aramaswamis@gmail.com>
2017-12-23 11:09:44 -0500

    display proper pages in reviewView

--- iasf/review/views/reviewView.py
+++ iasf/review/views/reviewView.py
@@ -1,9 +1,10 @@
 from iasf.review.mixins import UserIsStaffMixin
 from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 from iasf.apply.models import Application
 from iasf.apply.forms import ApplicationForm
-from django.views.generic.edit import UpdateView
+from django.views.generic.base import TemplateView
 from iasf.apply.schemas import JSONListFieldSchemas
 import json
 from django.db.models import AutoField
@@ -11,43 +12,36 @@ from django.core.exceptions import ValidationError
 from django.contrib import messages
 
 
-class ReviewView(UserIsStaffMixin, UpdateView):
+class ReviewView(UserIsStaffMixin, TemplateView):
     template_name = 'review/reviewView.html'
-    model = Application 
     JSONListFieldSchemas = json.dumps(JSONListFieldSchemas.schema)
     # success_url = reverse_lazy('apply:form-page', kwargs={'step':self.kwargs['step']})
     # form_class = ApplicationForm
 
     def dispatch(self, *args, **kwargs):
-        """Checks to see if an ApplicationForm is already present for the user;
-           if not, redirects them to the appropriate page.
-        """
         # todo: error handling here.
-        self.success_url = self.request.get_full_path()
-        self.form_pages = []
-        for step, page in enumerate(Application.pages):
-            try:
-                # step = int(self.kwargs['step'])
-                class ApplicationFormCustom(ApplicationForm):
-                    class Meta(ApplicationForm.Meta):
-                        fieldsets = Application.getFields(step)
-                self.form_class = ApplicationFormCustom
-                self.form_pages.append(ApplicationFormCustom)
-            except ValueError as verr:
-                return HttpResponseRedirect(reverse_lazy('apply:form-page-start'))
-            except Exception as ex:
-                return HttpResponseRedirect(reverse_lazy('apply:form-page-start'))
         try:
-            self.application = Application.objects.get(pk=self.kwargs['id'])
+            self.application = get_object_or_404(Application, pk=self.kwargs['id'])
             #self.object = self.application
         except Application.DoesNotExist:
-            return HttpResponseRedirect(reverse_lazy('apply:application-new'))
+            return HttpResponseNotFound('<h1>Application not found</h1>')
+        self.success_url = self.request.get_full_path()
+        self.form_pages = []
+        # self.fields['sku'].widget.attrs['readonly'] = True
+        for step, page in enumerate(Application.pages):
+            if (not self.application.finaid_applying_for and 'financialOnly' in page and page['financialOnly'] == True):
+                continue
+            class ApplicationFormCustom(ApplicationForm):
+                def __init__(self, *args, **kwargs):
+                    super(ApplicationFormCustom, self).__init__(*args, **kwargs)
+                    for field in self.fields:
+                        self.fields[field].widget.attrs['disabled'] = True
+                class Meta(ApplicationForm.Meta):
+                    fieldsets = Application.getFields(step)
+            self.form_class = ApplicationFormCustom
+            self.form_pages.append(ApplicationFormCustom(initial=self.application.__dict__))
         return super(ReviewView, self).dispatch(*args, **kwargs)
-    def get_object(self):
-        """Behaves the same as form_class attribute -- but this lets one
-           subclass ApplicationForm so that only certain fieldsets are displayed from it.
-        """
-        return self.application
+    
     def get_context_data(self, **kwargs):
         context = super(ReviewView, self).get_context_data(**kwargs)
         context['form_pages'] = self.form_pages
